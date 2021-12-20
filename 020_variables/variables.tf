4c4c4e37d8619a950dfdaf9d071d840fa01a6a3b
sanyatishenko <sanyatishenko@outlook.com>
2021-12-20 20:18:14 +0200

    Created all types of variables

--- 020_variables/variables.tf
+++ 020_variables/variables.tf
@@ -1,16 +1,18 @@
 /*
 
-Simple types:
-    string
-    number
-    bool
+Simple (Primitive) types:
+    string                                  A sequence of Unicode characters representing some text, such as "hello".
+    number                                  The number type can represent both whole numbers like 15 and fractional values such as 6.28.
+    bool                                    Either true or false. bool values can be used in conditional logic.
 
-Complex types:
-    list(<TYPE>)
-    set(<TYPE>)
-    map(<TYPE>)
-    object({<ATTR NAME> = <TYPE>, ... })
-    tuple([<TYPE>, ...])
+Collection types (All elements of a collection must always be of the same type): 
+    list(<TYPE>)                            A sequence of values of the same type.
+    set(<TYPE>)                             An unordered collection of unique values, all of the same type.
+    map(<TYPE>)                             A lookup table, matching keys to values, all of the same type.
+
+Structural types:
+    object({<ATTR NAME> = <TYPE>, ... })    A lookup table, matching a fixed set of keys to values of specified types.
+    tuple([<TYPE>, ...])                    A sequence of elements identified by consecutive whole numbers starting with zero, where each element has its own type.
 
 */
 
@@ -42,6 +44,14 @@ variable inbound_ports {
   description = "Opened ports"
 }
 
+# set
+variable sg_ids {
+  type        = set
+  default     = []
+  description = "Set of security groups"
+}
+
+
 # map
 variable tags {
   type        = map
@@ -52,3 +62,25 @@ variable tags {
   description = "Tegs for EC2 Instances"
 }
 
+# object
+variable ami {
+  type        = object({
+      ami_name = string,
+      virtualization_type = string,
+      owners = string
+  })
+  default     = {
+      ami_name = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
+      virtualization_type = "hvm"
+      owners = "099720109477"
+  }
+  description = "Object with ami parameters"
+}
+
+# tuple
+variable add_parameters {
+  type        = tuple[bool,bool]
+  # hibirnate, monitoring
+  default     = [false, false]
+  description = "Additional parameters"
+}
