a6bffb3b21ddcad2af0ee2356449121f01841610
sanyatishenko <sanyatishenko@outlook.com>
2021-12-21 21:43:07 +0200

    Add debugging

---
+++ 040_functions_logging/main.tf
@@ -0,0 +1,24 @@
+/*
+ Environment variables for debuging terraform:
+ 
+ TF_LOG = <LOG_LEVEL>  
+ TF_LOG_CORE = <LOG_LEVEL> 
+ TF_LOG_PROVIDER = <LOG_LEVEL> 
+
+log levels TRACE, DEBUG, INFO, WARN or ERROR
+
+ TF_LOG_PATH = <file_path>
+
+*/
+
+# Functions
+
+lookup
+split
+concat
+contains
+element
+merge
+file
+formatdate
+timestamp
