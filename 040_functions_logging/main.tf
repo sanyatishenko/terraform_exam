4a64f6e95347607704c63f76c4181804b9e2b817
sanyatishenko <sanyatishenko@outlook.com>
2021-12-22 21:28:34 +0200

    add some functions

--- 040_functions_logging/main.tf
+++ 040_functions_logging/main.tf
@@ -13,12 +13,70 @@ log levels TRACE, DEBUG, INFO, WARN or ERROR
 
 # Functions
 
-lookup
-split
-concat
-contains
-element
-merge
+variable map {
+  type        = map 
+  default     = {"https" = "443", "http" = "80", "ssh"="22"}
+  description = "Common ports"
+}
+
+variable string {
+  type        = string
+  default     = "ssh-http"
+  description = "String of ports"
+}
+
+/*
+# split(separator, string)
+split("-", var.string)
+tolist([
+  "ssh",
+  "http",
+])
+
+# element(list, index)
+element(split("-", var.string),0)
+"ssh"
+
+# lookup(map, key, default)
+lookup(var.map,element(split("-", var.string),0))
+"22"
+
+lookup(var.map,"rdp",0)
+"0"
+
+# contains(list, value)
+contains(split("-", var.string),"ssh")
+true
+
+
+# Merge - returns a single map or object that contains a merged set of elements from all arguments.
+merge({a="b", c="d"}, {e="f", c="z"})
+{
+  "a" = "b"
+  "c" = "z"   #! Important
+  "e" = "f"
+}
+
+# concat - takes two or more lists and combines them into a single list.
+concat(split("-", var.name), ["ssh","rdp"])
+[
+  "ssh",
+  "http",
+  "ssh",
+  "rdp",
+]
+
+
+# toset() - converts its argument to a set value.
+toset(concat(split("-", var.string), ["ssh","rdp"]))
+toset([
+  "http",
+  "rdp",
+  "ssh",
+])
+
+
 file
 formatdate
 timestamp
+*/
